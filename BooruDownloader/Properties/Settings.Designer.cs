8068029a5e7177e3919a19e662bbefef179f7f4b
TypeA2 <nuanlim29@gmail.com>
2020-07-24 02:56:48 +0200

    Update default output format

--- BooruDownloader/Properties/Settings.Designer.cs
+++ BooruDownloader/Properties/Settings.Designer.cs
@@ -12,7 +12,7 @@ namespace BooruDownloader.Properties {
     
     
     [global::System.Runtime.CompilerServices.CompilerGeneratedAttribute()]
-    [global::System.CodeDom.Compiler.GeneratedCodeAttribute("Microsoft.VisualStudio.Editors.SettingsDesigner.SettingsSingleFileGenerator", "15.9.0.0")]
+    [global::System.CodeDom.Compiler.GeneratedCodeAttribute("Microsoft.VisualStudio.Editors.SettingsDesigner.SettingsSingleFileGenerator", "16.6.0.0")]
     public sealed partial class Settings : global::System.Configuration.ApplicationSettingsBase {
         
         private static Settings defaultInstance = ((Settings)(global::System.Configuration.ApplicationSettingsBase.Synchronized(new Settings())));
@@ -49,7 +49,7 @@ namespace BooruDownloader.Properties {
         
         [global::System.Configuration.UserScopedSettingAttribute()]
         [global::System.Diagnostics.DebuggerNonUserCodeAttribute()]
-        [global::System.Configuration.DefaultSettingValueAttribute("MD5")]
+        [global::System.Configuration.DefaultSettingValueAttribute("TaggedFilename")]
         public string CurrentFormat {
             get {
                 return ((string)(this["CurrentFormat"]));
